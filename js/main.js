e9171d947d5644248550f814e1776300ac6bb84a
Patternscompany <pandureddypatterns@gmail.com>
2025-11-19 23:30:15 +0530

    pp

--- js/main.js
+++ js/main.js
@@ -38,6 +38,20 @@
         $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
         return false;
     });
+
+     // Testimonials carousel
+    $(".testimonial-carousel").owlCarousel({
+        items: 1,
+        autoplay: true,
+        smartSpeed: 1000,
+        dots: true,
+        loop: true,
+        nav: true,
+        navText : [
+            '<i class="bi bi-chevron-left"></i>',
+            '<i class="bi bi-chevron-right"></i>'
+        ]
+    });
     
 })(jQuery);
 
